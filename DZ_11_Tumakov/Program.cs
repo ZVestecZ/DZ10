45efced2b84984a8e51bee884ae3a0f0a0e6cd45
ZXCVestec <semabersenev@gmail.com>
2025-01-16 20:56:47 +0700

    a

---
+++ DZ_11_Tumakov/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_11_Tumakov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zov2
+        }
+    }
+}
